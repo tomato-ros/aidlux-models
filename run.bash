aab52567362cd15c2c52b7051f061410a11652b0
tomatoros <smartros@foxmail.com>
2026-08-13 23:01:33 +0800

    update

--- run.bash
+++ run.bash
@@ -26,17 +26,20 @@ fun_print_line(){
 export SOC=qcs8550
 
 fun_print_line "opus-mt-en-zh_qcs8550_fp16"
-
 ./$SOC/opus-mt-en-zh_qcs8550_fp16/run.bash
 
 
-
 fun_print_line "YOLOv8m_qcs8550_fp16"
-
 ./$SOC/YOLOv8m_qcs8550_fp16/run.bash
 
 
-
 fun_print_line "WeTTS_qcs8550_fp32"
-
 ./$SOC/WeTTS_qcs8550_fp32/run.bash
+
+
+fun_print_line "Qwen3-VL-8B-Instruct"
+./$SOC/qnn248_qcs8550_cl4096/run.bash
+
+
+
+
