4d7c5fd4cda896f35f25a2d1d6705c8da68be5d6
tomatoros <smartros@foxmail.com>
2026-08-13 16:13:01 +0800

    修复资源未释放错误

--- qcs8550/opus-mt-en-zh_qcs8550_fp16/code/aidlite_tool.py
+++ qcs8550/opus-mt-en-zh_qcs8550_fp16/code/aidlite_tool.py
@@ -1,25 +1,37 @@
-import os 
-import pdb
-import aidlite
 import time
+
+import aidlite
 import numpy as np
+
+
 class AidliteTool:
-    def load_encoder(self,model_path):
+
+    def load_encoder(self, model_path):
+
         print(model_path)
+
         config = aidlite.Config.create_instance()
         if config is None:
             print("Create config failed !")
             return False
+
         config.implement_type = aidlite.ImplementType.TYPE_LOCAL
         config.framework_type = aidlite.FrameworkType.TYPE_QNN248
         config.accelerate_type = aidlite.AccelerateType.TYPE_DSP
         config.is_quantify_model = 1
+
         model = aidlite.Model.create_instance(model_path)
-        self.input_shapes=[[1, 256],[1, 256]]
-        self.output_shapes = [[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64],[1,8,256,64]]
+
+        self.input_shapes = [[1, 256], [1, 256]]
+        self.output_shapes = [[1, 8, 256, 64], [1, 8, 256, 64], [1, 8, 256, 64], [1, 8, 256, 64], [1, 8, 256, 64],
+                              [1, 8, 256, 64], [1, 8, 256, 64], [1, 8, 256, 64], [1, 8, 256, 64], [1, 8, 256, 64],
+                              [1, 8, 256, 64], [1, 8, 256, 64]]
+
         model.set_model_properties(self.input_shapes, aidlite.DataType.TYPE_FLOAT32,
                                    self.output_shapes, aidlite.DataType.TYPE_FLOAT32)
+
         self.interpreter = aidlite.InterpreterBuilder.build_interpretper_from_model_and_config(model, config)
+
         if self.interpreter is None:
             print("build_interpretper_from_model_and_config failed !")
             return None
@@ -32,55 +44,70 @@ class AidliteTool:
             print("interpreter load model failed !")
             return False
         return self.interpreter
-    def load_decoder(self,model_path):
+
+    def load_decoder(self, model_path):
         print(model_path)
+
         config = aidlite.Config.create_instance()
+
         if config is None:
             print("Create config failed !")
             return False
+
         config.implement_type = aidlite.ImplementType.TYPE_LOCAL
         config.framework_type = aidlite.FrameworkType.TYPE_QNN248
         config.accelerate_type = aidlite.AccelerateType.TYPE_DSP
         config.is_quantify_model = 1
+
         model = aidlite.Model.create_instance(model_path)
-        self.input_shapes=[[1,8,255,64],[1,8,255,64],[1,8,255,64],[1,8,255,64],
-                           [1,8,255,64],[1,8,255,64],[1,8,255,64],[1,8,255,64],
-                           [1,8,255,64],[1,8,255,64],[1,8,255,64],[1,8,255,64],
-                           [1,8,255,64],[1,8,255,64],[1,8,255,64],[1,8,255,64],
-                           [1,8,255,64],[1,8,255,64],[1,8,255,64],[1,8,255,64],
-                           [1,8,255,64],[1,8,255,64],[1,8,255,64],[1,8,255,64],
-                           [1, 256],[1, 1],[1],]
-        
-        self.output_shapes = [[1,8,1,64],[1,8,1,64],
-                              [1,8,1,64],[1,8,1,64],
-                              [1,8,1,64],[1,8,1,64],
-                              [1,8,1,64],[1,8,1,64],
-                              [1,8,1,64],[1,8,1,64],
-                              [1,8,1,64],[1,8,1,64],
-                              [1,1,65001]]
+
+        self.input_shapes = [[1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64],
+                             [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64],
+                             [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64],
+                             [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64],
+                             [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64],
+                             [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64], [1, 8, 255, 64],
+                             [1, 256], [1, 1], [1], ]
+
+        self.output_shapes = [[1, 8, 1, 64], [1, 8, 1, 64],
+                              [1, 8, 1, 64], [1, 8, 1, 64],
+                              [1, 8, 1, 64], [1, 8, 1, 64],
+                              [1, 8, 1, 64], [1, 8, 1, 64],
+                              [1, 8, 1, 64], [1, 8, 1, 64],
+                              [1, 8, 1, 64], [1, 8, 1, 64],
+                              [1, 1, 65001]]
+
         model.set_model_properties(self.input_shapes, aidlite.DataType.TYPE_FLOAT32,
                                    self.output_shapes, aidlite.DataType.TYPE_FLOAT32)
+
         self.interpreter = aidlite.InterpreterBuilder.build_interpretper_from_model_and_config(model, config)
+
         if self.interpreter is None:
             print("build_interpretper_from_model_and_config failed !")
             return None
         result = self.interpreter.init()
+
         if result != 0:
             print(f"interpreter init failed !")
             return False
+
         result = self.interpreter.load_model()
+
         if result != 0:
             print("interpreter load model failed !")
             return False
+
         return self.interpreter
-    def init(self,encoder_model_path,decoder_model_path):
-        self.encoder=self.load_encoder(encoder_model_path)
-        self.decoder=self.load_decoder(decoder_model_path)
-    def decoder_infer(self,data):
+
+    def init(self, encoder_model_path, decoder_model_path):
+        self.encoder = self.load_encoder(encoder_model_path)
+        self.decoder = self.load_decoder(decoder_model_path)
+
+    def decoder_infer(self, data):
         result = self.decoder.set_input_tensor(0, data["input_ids"].astype(np.float32).data)
         result = self.decoder.set_input_tensor(1, data["encoder_attention_mask"].astype(np.float32).data)
         result = self.decoder.set_input_tensor(2, data["position"].astype(np.float32).data)
-        
+
         result = self.decoder.set_input_tensor(3, data["block_0_past_self_key_states"].astype(np.float32).data)
         result = self.decoder.set_input_tensor(4, data["block_0_past_self_value_states"].astype(np.float32).data)
         result = self.decoder.set_input_tensor(5, data["block_0_cross_key_states"].astype(np.float32).data)
@@ -111,86 +138,94 @@ class AidliteTool:
         result = self.decoder.set_input_tensor(25, data["block_5_cross_key_states"].astype(np.float32).data)
         result = self.decoder.set_input_tensor(26, data["block_5_cross_value_states"].astype(np.float32).data)
 
-
-
-
         for i in range(1):
-            begin_time=time.time()
-            result = self.decoder.invoke()
-            print("decoder time:",time.time()-begin_time)
-        res_list=[]
+            begin_time = time.time()
+            self.decoder.invoke()
+            print("解码时间:{0}".format(time.time() - begin_time))
 
-        logits=self.decoder.get_output_tensor(12).reshape(1,1,65001)
+        res_list = []
+
+        logits = self.decoder.get_output_tensor(12).reshape(1, 1, 65001)
         res_list.append(logits)
 
-        block_0_present_self_key_states=self.decoder.get_output_tensor(0).reshape(1,8,1,64)
-        block_0_present_self_value_states=self.decoder.get_output_tensor(1).reshape(1,8,1,64)
+        block_0_present_self_key_states = self.decoder.get_output_tensor(0).reshape(1, 8, 1, 64)
+        block_0_present_self_value_states = self.decoder.get_output_tensor(1).reshape(1, 8, 1, 64)
         res_list.append(block_0_present_self_key_states)
         res_list.append(block_0_present_self_value_states)
 
-        block_1_present_self_key_states=self.decoder.get_output_tensor(2).reshape(1,8,1,64)
-        block_1_present_self_value_states=self.decoder.get_output_tensor(3).reshape(1,8,1,64)
+        block_1_present_self_key_states = self.decoder.get_output_tensor(2).reshape(1, 8, 1, 64)
+        block_1_present_self_value_states = self.decoder.get_output_tensor(3).reshape(1, 8, 1, 64)
         res_list.append(block_1_present_self_key_states)
         res_list.append(block_1_present_self_value_states)
 
-        block_2_present_self_key_states=self.decoder.get_output_tensor(4).reshape(1,8,1,64)
-        block_2_present_self_value_states=self.decoder.get_output_tensor(5).reshape(1,8,1,64)
+        block_2_present_self_key_states = self.decoder.get_output_tensor(4).reshape(1, 8, 1, 64)
+        block_2_present_self_value_states = self.decoder.get_output_tensor(5).reshape(1, 8, 1, 64)
         res_list.append(block_2_present_self_key_states)
         res_list.append(block_2_present_self_value_states)
 
-        block_3_present_self_key_states=self.decoder.get_output_tensor(6).reshape(1,8,1,64)
-        block_3_present_self_value_states=self.decoder.get_output_tensor(7).reshape(1,8,1,64)
+        block_3_present_self_key_states = self.decoder.get_output_tensor(6).reshape(1, 8, 1, 64)
+        block_3_present_self_value_states = self.decoder.get_output_tensor(7).reshape(1, 8, 1, 64)
         res_list.append(block_3_present_self_key_states)
         res_list.append(block_3_present_self_value_states)
 
-        block_4_present_self_key_states=self.decoder.get_output_tensor(8).reshape(1,8,1,64)
-        block_4_present_self_value_states=self.decoder.get_output_tensor(9).reshape(1,8,1,64)
+        block_4_present_self_key_states = self.decoder.get_output_tensor(8).reshape(1, 8, 1, 64)
+        block_4_present_self_value_states = self.decoder.get_output_tensor(9).reshape(1, 8, 1, 64)
         res_list.append(block_4_present_self_key_states)
         res_list.append(block_4_present_self_value_states)
 
-        block_5_present_self_key_states=self.decoder.get_output_tensor(10).reshape(1,8,1,64)
-        block_5_present_self_value_states=self.decoder.get_output_tensor(11).reshape(1,8,1,64)
+        block_5_present_self_key_states = self.decoder.get_output_tensor(10).reshape(1, 8, 1, 64)
+        block_5_present_self_value_states = self.decoder.get_output_tensor(11).reshape(1, 8, 1, 64)
         res_list.append(block_5_present_self_key_states)
         res_list.append(block_5_present_self_value_states)
 
         return res_list
 
-    def encoder_infer(self,data):
+    def encoder_infer(self, data):
         # pdb.set_trace()
         result = self.encoder.set_input_tensor(0, data["input_ids"].astype(np.float32).data)
         result = self.encoder.set_input_tensor(1, data["encoder_attention_mask"].astype(np.float32).data)
+
         for i in range(1):
-            begin_time=time.time()
-            result = self.encoder.invoke()
-            print("encoder time:",time.time()-begin_time)
-        res_list=[]
-        block_0_cross_key_states=self.encoder.get_output_tensor(0).reshape(1,8,256,64)
-        block_0_cross_value_states=self.encoder.get_output_tensor(1).reshape(1,8,256,64)
+            begin_time = time.time()
+
+            self.encoder.invoke()
+
+            print('--------------------------------------------------------------')
+            print("编码时间:", time.time() - begin_time)
+
+        res_list = []
+        block_0_cross_key_states = self.encoder.get_output_tensor(0).reshape(1, 8, 256, 64)
+        block_0_cross_value_states = self.encoder.get_output_tensor(1).reshape(1, 8, 256, 64)
         res_list.append(block_0_cross_key_states)
         res_list.append(block_0_cross_value_states)
 
-        block_1_cross_key_states=self.encoder.get_output_tensor(2).reshape(1,8,256,64)
-        block_1_cross_value_states=self.encoder.get_output_tensor(3).reshape(1,8,256,64)
+        block_1_cross_key_states = self.encoder.get_output_tensor(2).reshape(1, 8, 256, 64)
+        block_1_cross_value_states = self.encoder.get_output_tensor(3).reshape(1, 8, 256, 64)
         res_list.append(block_1_cross_key_states)
         res_list.append(block_1_cross_value_states)
-        
-        block_2_cross_key_states=self.encoder.get_output_tensor(4).reshape(1,8,256,64)
-        block_2_cross_value_states=self.encoder.get_output_tensor(5).reshape(1,8,256,64)
+
+        block_2_cross_key_states = self.encoder.get_output_tensor(4).reshape(1, 8, 256, 64)
+        block_2_cross_value_states = self.encoder.get_output_tensor(5).reshape(1, 8, 256, 64)
         res_list.append(block_2_cross_key_states)
         res_list.append(block_2_cross_value_states)
-        
-        block_3_cross_key_states=self.encoder.get_output_tensor(6).reshape(1,8,256,64)
-        block_3_cross_value_states=self.encoder.get_output_tensor(7).reshape(1,8,256,64)
+
+        block_3_cross_key_states = self.encoder.get_output_tensor(6).reshape(1, 8, 256, 64)
+        block_3_cross_value_states = self.encoder.get_output_tensor(7).reshape(1, 8, 256, 64)
         res_list.append(block_3_cross_key_states)
         res_list.append(block_3_cross_value_states)
-        
-        block_4_cross_key_states=self.encoder.get_output_tensor(8).reshape(1,8,256,64)
-        block_4_cross_value_states=self.encoder.get_output_tensor(9).reshape(1,8,256,64)
+
+        block_4_cross_key_states = self.encoder.get_output_tensor(8).reshape(1, 8, 256, 64)
+        block_4_cross_value_states = self.encoder.get_output_tensor(9).reshape(1, 8, 256, 64)
         res_list.append(block_4_cross_key_states)
         res_list.append(block_4_cross_value_states)
-        
-        block_5_cross_key_states=self.encoder.get_output_tensor(10).reshape(1,8,256,64)
-        block_5_cross_value_states=self.encoder.get_output_tensor(11).reshape(1,8,256,64)
+
+        block_5_cross_key_states = self.encoder.get_output_tensor(10).reshape(1, 8, 256, 64)
+        block_5_cross_value_states = self.encoder.get_output_tensor(11).reshape(1, 8, 256, 64)
         res_list.append(block_5_cross_key_states)
         res_list.append(block_5_cross_value_states)
+
         return res_list
+
+    def destory(self):
+        self.encoder.destroy()
+        self.decoder.destroy()
--- qcs8550/opus-mt-en-zh_qcs8550_fp16/code/main.py
+++ qcs8550/opus-mt-en-zh_qcs8550_fp16/code/main.py
@@ -1,28 +1,31 @@
-from transformers import MarianTokenizer
-import onnxruntime as ort
 import numpy as np
-import os
-from aidlite_tool import AidliteTool
-import pdb
+from transformers import MarianTokenizer
 
-def get_acc(onnx_out,other_out):
-    cosine_similarity=np.dot(np.array(onnx_out),np.array(other_out))/(np.linalg.norm(np.array(onnx_out)) * np.linalg.norm(np.array(other_out)))
+from aidlite_tool import AidliteTool
+
+
+def get_acc(onnx_out, other_out):
+    cosine_similarity = np.dot(np.array(onnx_out), np.array(other_out)) / (
+            np.linalg.norm(np.array(onnx_out)) * np.linalg.norm(np.array(other_out)))
     return cosine_similarity
 
-aid_tool=AidliteTool()
 
-decoder_model_path="models/QCS8550/FP16/opus_mt_decoder_htp.bin"
-encoder_model_path="models/QCS8550/FP16/opus_mt_encoder_htp.bin"
+aid_tool = AidliteTool()
 
-aid_tool.init(encoder_model_path,decoder_model_path)
+decoder_model_path = "models/QCS8550/FP16/opus_mt_decoder_htp.bin"
+encoder_model_path = "models/QCS8550/FP16/opus_mt_encoder_htp.bin"
 
-MAX_SEQ_LEN_ENC = 256
-MAX_SEQ_LEN_DEC = 256
+aid_tool.init(encoder_model_path, decoder_model_path)
+
+MAX_SEQ_LEN_ENC = 256  # default 256
+MAX_SEQ_LEN_DEC = 256  # default 256
 TRANSPOSE_KEY = False
 
 NUM_LAYERS = 6
-#NUM_HEADS = 8
-#HEAD_DIM = 64
+
+
+# NUM_HEADS = 8
+# HEAD_DIM = 64
 
 class OnnxOpusMtEncoder:
     def __init__(self):
@@ -49,17 +52,18 @@ class OnnxOpusMtEncoder:
             output_names.append(f"block_{layer_idx}_cross_value_states")
         # outputs = self.session.run(output_names, inputs)
         # for i in range(len(outputs)):
-            # print(outputs[i][0,0,0,0])
-        aid_res=aid_tool.encoder_infer(inputs)
+        # print(outputs[i][0,0,0,0])
+        aid_res = aid_tool.encoder_infer(inputs)
         # print("/"*10)
         # for i in range(len(aid_res)):
-            # print(aid_res[i][0,0,0,0])
+        # print(aid_res[i][0,0,0,0])
         # for i in range(len(aid_res)):
-            # acc=get_acc(outputs[i].flatten(),aid_res[i].flatten())
-            # print("acc:",acc,end=" ")
+        # acc=get_acc(outputs[i].flatten(),aid_res[i].flatten())
+        # print("acc:",acc,end=" ")
         # pdb.set_trace()
         # print("")
         return aid_res
+
 
 class OnnxOpusMtDecoder:
     def __init__(self):
@@ -73,10 +77,10 @@ class OnnxOpusMtDecoder:
             'position': position,
         }
         for layer_idx in range(NUM_LAYERS):
-            inputs[f"block_{layer_idx}_past_self_key_states"] = past_key_values[4*layer_idx]
-            inputs[f"block_{layer_idx}_past_self_value_states"] = past_key_values[4*layer_idx+1]
-            inputs[f"block_{layer_idx}_cross_key_states"] = past_key_values[4*layer_idx+2]
-            inputs[f"block_{layer_idx}_cross_value_states"] = past_key_values[4*layer_idx+3]
+            inputs[f"block_{layer_idx}_past_self_key_states"] = past_key_values[4 * layer_idx]
+            inputs[f"block_{layer_idx}_past_self_value_states"] = past_key_values[4 * layer_idx + 1]
+            inputs[f"block_{layer_idx}_cross_key_states"] = past_key_values[4 * layer_idx + 2]
+            inputs[f"block_{layer_idx}_cross_value_states"] = past_key_values[4 * layer_idx + 3]
 
         # input_list = ""
         # os.makedirs(f"decoder_inputs", exist_ok=True)
@@ -92,35 +96,49 @@ class OnnxOpusMtDecoder:
             output_names.append(f"block_{layer_idx}_present_self_value_states")
 
         # outputs = self.session.run(output_names, inputs)
-        aid_res=aid_tool.decoder_infer(inputs)
-        
+        aid_res = aid_tool.decoder_infer(inputs)
+
         # print("/"*10)
         # for i in range(len(aid_res)):
-            # acc=get_acc(outputs[i].flatten(),aid_res[i].flatten())
-            # print(i," acc:",acc," ",outputs[i].shape,aid_res[i].shape)
+        # acc=get_acc(outputs[i].flatten(),aid_res[i].flatten())
+        # print(i," acc:",acc," ",outputs[i].shape,aid_res[i].shape)
 
         return aid_res
 
-intermediates_path="./model_en2zh"
+
+intermediates_path = "./model_en2zh"
 model_name = './code/opus-mt-en-zh'
 
-encoder  = OnnxOpusMtEncoder()
-decoder  = OnnxOpusMtDecoder()
+encoder = OnnxOpusMtEncoder()
+decoder = OnnxOpusMtDecoder()
 
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 
-# Example translation
-# src_texts = ["床前明月光， 疑是地上霜。 举头望明月， 低头思故乡。"]
-# src_texts = ["今天的天气怎么样？"]
-src_texts = ["How is the weather today?"]
+src_texts = ["""
+
+Features
+
+Modularity based on hardware accelerators
+
+The Qualcomm® AI Engine Direct architecture is designed to be modular and allows for clean separation in the software 
+for different hardware cores/accelerators, such as the CPU, GPU, and DSP that are designated as backends.
+
+Learn more about the Qualcomm® AI Engine Direct backends here.
+"""]
+
 inputs = tokenizer(src_texts, return_tensors="pt", padding=True)
+
+print('--------------------------------------------------------------')
+
 print(inputs)
 
-encoder_input_ids = np.zeros([1,MAX_SEQ_LEN_ENC], dtype=np.int32)
-encoder_attention_mask = np.zeros([1,MAX_SEQ_LEN_ENC], dtype=np.int32)
+print('--------------------------------------------------------------')
+
+encoder_input_ids = np.zeros([1, MAX_SEQ_LEN_ENC], dtype=np.int32)
+encoder_attention_mask = np.zeros([1, MAX_SEQ_LEN_ENC], dtype=np.int32)
 for idx in range(inputs.input_ids.shape[1]):
-    encoder_input_ids[0,idx] = inputs.input_ids[0,idx]
-    encoder_attention_mask[0,idx] = 1
+    encoder_input_ids[0, idx] = inputs.input_ids[0, idx]
+    encoder_attention_mask[0, idx] = 1
 
 enc_out = encoder.forward(input_ids=encoder_input_ids, encoder_attention_mask=encoder_attention_mask)
 
@@ -128,24 +146,31 @@ enc_out = encoder.forward(input_ids=encoder_input_ids, encoder_attention_mask=en
 past_key_values = []
 for layer_idx in range(NUM_LAYERS):
     if TRANSPOSE_KEY:
-        past_key_values.append(np.zeros([enc_out[0].shape[0], enc_out[0].shape[1], enc_out[0].shape[2], MAX_SEQ_LEN_DEC-1], dtype=np.float32))
+        past_key_values.append(
+            np.zeros([enc_out[0].shape[0], enc_out[0].shape[1], enc_out[0].shape[2], MAX_SEQ_LEN_DEC - 1],
+                     dtype=np.float32))
     else:
-        past_key_values.append(np.zeros([enc_out[0].shape[0], enc_out[0].shape[1], MAX_SEQ_LEN_DEC-1, enc_out[0].shape[3]], dtype=np.float32))
-    past_key_values.append(np.zeros([enc_out[1].shape[0], enc_out[1].shape[1], MAX_SEQ_LEN_DEC-1, enc_out[1].shape[3]], dtype=np.float32))
-    past_key_values.append(enc_out[2*layer_idx])
-    past_key_values.append(enc_out[2*layer_idx+1])
+        past_key_values.append(
+            np.zeros([enc_out[0].shape[0], enc_out[0].shape[1], MAX_SEQ_LEN_DEC - 1, enc_out[0].shape[3]],
+                     dtype=np.float32))
+    past_key_values.append(
+        np.zeros([enc_out[1].shape[0], enc_out[1].shape[1], MAX_SEQ_LEN_DEC - 1, enc_out[1].shape[3]],
+                 dtype=np.float32))
+    past_key_values.append(enc_out[2 * layer_idx])
+    past_key_values.append(enc_out[2 * layer_idx + 1])
 
-decoder_input_ids = np.zeros([1,1], dtype=np.int32)
+decoder_input_ids = np.zeros([1, 1], dtype=np.int32)
 
 token = 65000
-decoder_input_ids[0,0] = token
+decoder_input_ids[0, 0] = token
 
 tokens = []
 
-for idx in range(MAX_SEQ_LEN_DEC-1):
-    logits, *present_key_values = decoder.forward(decoder_input_ids, encoder_attention_mask, np.array(idx).astype(np.int32), *past_key_values)
+for idx in range(MAX_SEQ_LEN_DEC - 1):
+    logits, *present_key_values = decoder.forward(decoder_input_ids, encoder_attention_mask,
+                                                  np.array(idx).astype(np.int32), *past_key_values)
 
-    token = np.argmax(logits, -1)[0,0]
+    token = np.argmax(logits, -1)[0, 0]
     # check for end of sequence
     if (token == 0):
         break
@@ -153,14 +178,25 @@ for idx in range(MAX_SEQ_LEN_DEC-1):
     tokens.append(token)
 
     # set up input for next time
-    decoder_input_ids[0,0] = token
+    decoder_input_ids[0, 0] = token
 
     for layer_idx in range(NUM_LAYERS):
         if TRANSPOSE_KEY:
-            past_key_values[4*layer_idx][:,:,:,idx:idx+1] = present_key_values[2*layer_idx]
+            past_key_values[4 * layer_idx][:, :, :, idx:idx + 1] = present_key_values[2 * layer_idx]
         else:
-            past_key_values[4*layer_idx][:,:,idx:idx+1,:] = present_key_values[2*layer_idx]
-        past_key_values[4*layer_idx+1][:,:,idx:idx+1,:] = present_key_values[2*layer_idx+1]
+            past_key_values[4 * layer_idx][:, :, idx:idx + 1, :] = present_key_values[2 * layer_idx]
+        past_key_values[4 * layer_idx + 1][:, :, idx:idx + 1, :] = present_key_values[2 * layer_idx + 1]
+
+print('--------------------------------------------------------------')
 
 print(tokens)
-print(tokenizer.decode(tokens, skip_special_tokens=True))
+
+print('--------------------------------------------------------------')
+
+trans_result = tokenizer.decode(tokens, skip_special_tokens=True)
+
+print(trans_result)
+
+print('--------------------------------------------------------------')
+
+aid_tool.destory()
